9f1e081788599f0c604e529c154ac26279a4e4ff
Oleksandr_Baglai <apofig@gmail.com>
2020-08-27 18:00:46 +0300

    [portable][linux]Сделал скрипт, который будет предлагать в user friendly режиме править .env файл, перегенеривать базовые пароли, а так же скрипт, которым можно будет поднять коденжой вообще одной командой

---
+++ setup-server.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+if [ "$EUID" -ne 0 ]
+  then echo "[91mPlease run as root[0m"
+  exit
+fi
+
+eval_echo() {
+    to_run=$1
+    echo "[94m"
+    echo $to_run
+    echo "[0m"
+
+    eval $to_run
+}
+
+if [ -x "$(command -v git)" ]; then
+    echo "[Git installed[0m" ;
+else
+    eval_echo "sudo apt update"
+    eval_echo "sudo apt install git"
+    eval_echo "git --version"
+fi
+
+eval_echo "git clone https://github.com/codenjoyme/codenjoy-portable-linux.git codenjoy"
+eval_echo "cd ./codenjoy"
+
+eval_echo ". update-env.sh"
+eval_echo ". rebuild.sh"
---
+++ update-env.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+echo "[94mProcessing '.env' file line by line. Enter the new value, or press ENTER for the default.[0m"
+
+for entry in $(cat ./.env)
+do
+    if [[ -z "${entry// }" ]]; then
+        continue
+    fi
+
+    if [[ $entry == \#* ]]; then
+        continue
+    fi
+
+    key=$(echo $entry | awk -F= '{print $1}')
+    value=$(echo $entry | awk -F= '{print $2}')
+
+    if [[ $value == *"Password"* ]]; then
+        new_password=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
+        value=$new_password
+    fi
+
+    echo "[94m$key[0m=[91m$value[0m"
+
+    read -p "[94m$key[0m=[91m" new_value
+
+    echo "[0m"
+
+    if ! [[ -z "${new_value// }" ]]; then
+        value=$new_value
+    fi
+
+    sed -i '.env' -e "/^$key=/s/=.*/=$value/"
+
+    echo ""
+done
